1db14f7fa2924a235f61fb7111024d863512fd35
kamenjan <rok.arih@gmail.com>
2018-11-05 16:04:47 +0100

    add time table and update text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-// import TimeTable from './../TimeTable/TimeTable'
+import TimeTable from './../TimeTable/TimeTable'
 
 import './about.css'
 
@@ -10,20 +10,21 @@ const About = () => {
     <div id={'about'}>
 
       <div id={"description-container"}>
-        <p>Just curious what the fuss is all about? No problem. This event is
-          welcoming to all skill levels.</p>
-        <p>It will be a great opportunity to see
-          what other React developers are busy with, compare tool belts and
-          maybe even add some new tools to it.</p>
-        <p>Available for work? I'm sure somebody will gladly have a word
-          with you.</p>
+        <p>Just curious what the fuss is all about? No problem. Event is
+          welcoming to all skill levels and will include a short presentation
+          of React.</p>
+        <p>It will also be a great opportunity to see what other React
+          developers are busy with, compare tool belts and add some new tools
+          to it.</p>
+        <TimeTable/>
         <p>Poligon was really friendly and on board with the idea so they have
           set us up with an ideal location and environment at their Creative
-          Centre. Gals and guys at PovioLabs were also supper supportive and
-          will make sure no one leaves with an empty belly or dry mouth!</p>
-        <p>Always best to take it one step at the time, but with your help next
+          Centre. PovioLabs were also very supportive and will make sure you
+          won't have to leave with an empty belly or a dry mouth.</p>
+        <p>Always best to take it one step at a time, but with your help next
           step could be a proper React community.</p>
-        <p>Come join us.</p>
+        <p>Come join us on 13th of November 2018 at 6:00 PM in Creative Centre
+          Poligon, Tobačna ulica 5, 1000 Ljubljana.</p>
       </div>
 
       {/*<TimeTable/>*/}
--- src/components/TimeTable/TimeTable.js
+++ src/components/TimeTable/TimeTable.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
+import './timetable.css'
+
 const TimeTable = () => {
 
   return(
     <div id={'time-table-container'}>
       <ul>
-        <li>18.00 Intro</li>
-        <li>18.10 Next.js & MobX</li>
-        <li>18.30 TBD from Toshl</li>
-        <li>??.?? React and GSAP (Me?)</li>
-        <li>19.00 Word from our sponsors</li>
-        <li>19.30 Food & Drinks</li>
+        <li>6.00 // Intro</li>
+        <li>6.05 // What can React library do for you? (Rok Arih)</li>
+        <li>6.25 // The Next(.js) Best Thing (Adam Giacomelli, Anej Gorkič)</li>
+        <li>7.20 // Word from our sponsors</li>
+        <li>7.30 // Relax and share a word with fellow developers over a slice of pizza</li>
       </ul>
     </div>
   )
